0988f84bb40bccbbf862c79fc5d57b1f474b7d40
ShikaShikaNara <mihail.prima@mail.ru>
2023-06-22 13:39:42 +0300

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 
 import Contact from "./components/Contact"
 import Hero from "./components/Hero"
-import Test from "./components/Test"
 import Who from "./components/Who"
 import Works from "./components/Works"
 import styled from "styled-components"
